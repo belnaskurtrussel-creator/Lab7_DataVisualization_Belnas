08acd6bcd72232d9b616bb41631059a79a628cfa
belnaskurtrussel-creator <belnaskurtrussel@gmail.com>
2026-04-27 09:51:10 +0800

    Describe what you changed

--- Data&Observation7.py
+++ Data&Observation7.py
@@ -44,6 +44,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+df = pd.read_csv("spotify_top_1000_tracks.csv")
+
 print(df.columns)  # IMPORTANT: check actual columns
 
 # If release_date exists
